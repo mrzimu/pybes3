fix: Update data types for various constants and variables to avoid unexpected overflow

src/pybes3/detectors/emc.py
@@ -29,9 +29,9 @@ BARREL_L = 28.0
 with np.load(EMC_GEOM) as f:
     _emc_geom = dict(f)
 
-_part = _emc_geom["part"]
-_theta = _emc_geom["theta"]
-_phi = _emc_geom["phi"]
+_part = _emc_geom["part"].astype(np.int64)
+_theta = _emc_geom["theta"].astype(np.int64)
+_phi = _emc_geom["phi"].astype(np.int64)
 _points_x = _emc_geom["points_x"]
 _points_y = _emc_geom["points_y"]
 _points_z = _emc_geom["points_z"]
@@ -140,34 +140,34 @@ def get_emc_gid(part: IntLike, theta: IntLike, phi: IntLike) -> IntLike:
     if part == 0:
         res = 0
         if theta == 0 or theta == 1:
-            return np.uint16(theta * ENDCAP_PHI_01 + phi)
+            return theta * ENDCAP_PHI_01 + phi
 
         res += 2 * ENDCAP_PHI_01
         if theta == 2 or theta == 3:
-            return np.uint16(res + (theta - 2) * ENDCAP_PHI_23 + phi)
+            return res + (theta - 2) * ENDCAP_PHI_23 + phi
 
         res += 2 * ENDCAP_PHI_23
         if theta == 4 or theta == 5:
-            return np.uint16(res + (theta - 4) * ENDCAP_PHI_45 + phi)
+            return res + (theta - 4) * ENDCAP_PHI_45 + phi
 
     if part == 1:
-        return np.uint16(ENDCAP_CRYSTALS + theta * BARREL_PHI + phi)
+        return ENDCAP_CRYSTALS + theta * BARREL_PHI + phi
 
     if part == 2:
         res = ENDCAP_CRYSTALS + BARREL_CRYSTALS
 
         if theta == 4 or theta == 5:
-            return np.uint16(res + (5 - theta) * ENDCAP_PHI_45 + phi)
+            return res + (5 - theta) * ENDCAP_PHI_45 + phi
 
         res += 2 * ENDCAP_PHI_45
         if theta == 2 or theta == 3:
-            return np.uint16(res + (3 - theta) * ENDCAP_PHI_23 + phi)
+            return res + (3 - theta) * ENDCAP_PHI_23 + phi
 
         res += 2 * ENDCAP_PHI_23
         if theta == 0 or theta == 1:
-            return np.uint16(res + (1 - theta) * ENDCAP_PHI_01 + phi)
+            return res + (1 - theta) * ENDCAP_PHI_01 + phi
 
-    return np.uint16(65535)
+    return -1
 
 
 @nb.vectorize(cache=True)

src/pybes3/detectors/tof.py
@@ -18,9 +18,9 @@ N_PHI_OR_STRIP.setflags(write=False)
 
 
 def _init():
-    _part = np.empty(N_STRIPS, dtype=np.uint8)
-    _layer_or_module = np.empty(N_STRIPS, dtype=np.uint8)
-    _phi_or_strip = np.empty(N_STRIPS, dtype=np.uint8)
+    _part = np.empty(N_STRIPS, dtype=np.int64)
+    _layer_or_module = np.empty(N_STRIPS, dtype=np.int64)
+    _phi_or_strip = np.empty(N_STRIPS, dtype=np.int64)
 
     gid = 0
     for part in range(5):