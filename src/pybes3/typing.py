6b5420febc72f59d80bd4def9ddd0b6c6b6d60bc
Mingrun Li <mrli@ihep.ac.cn>
2026-08-19 18:04:53 +0800

    Revert "drop usage of `Union`"
    This reverts commit 4ce2f44e9c993086bb53fc38f7be97274eb26a63.

--- src/pybes3/typing.py
+++ src/pybes3/typing.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 
+from typing import Union
+
 import awkward as ak
 import numpy as np
 
-IntLike = ak.Array | np.ndarray | int | np.integer
-FloatLike = ak.Array | np.ndarray | float | np.floating
-BoolLike = ak.Array | np.ndarray | bool | np.bool_
+IntLike = Union[ak.Array, np.ndarray, int, np.integer]
+FloatLike = Union[ak.Array, np.ndarray, float, np.floating]
+BoolLike = Union[ak.Array, np.ndarray, bool, np.bool_]
