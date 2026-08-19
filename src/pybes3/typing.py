4ce2f44e9c993086bb53fc38f7be97274eb26a63
Mingrun Li <mrli@ihep.ac.cn>
2026-08-19 17:47:11 +0800

    drop usage of `Union`

--- src/pybes3/typing.py
+++ src/pybes3/typing.py
@@ -1,10 +1,8 @@
 from __future__ import annotations
 
-from typing import Union
-
 import awkward as ak
 import numpy as np
 
-IntLike = Union[ak.Array, np.ndarray, int, np.integer]
-FloatLike = Union[ak.Array, np.ndarray, float, np.floating]
-BoolLike = Union[ak.Array, np.ndarray, bool, np.bool_]
+IntLike = ak.Array | np.ndarray | int | np.integer
+FloatLike = ak.Array | np.ndarray | float | np.floating
+BoolLike = ak.Array | np.ndarray | bool | np.bool_
